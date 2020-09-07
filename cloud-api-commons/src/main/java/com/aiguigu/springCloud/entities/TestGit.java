5d067d8e629c50677cf8bf6e52a54d67e45828cf
lizhenxiangi <1789701052@qq.com>
2020-09-07 14:17:50 +0800

    第一次测试提交

---
+++ cloud-api-commons/src/main/java/com/aiguigu/springCloud/entities/TestGit.java
@@ -0,0 +1,13 @@
+package com.aiguigu.springCloud.entities;
+
+/**
+ * @ClassName: TestGit
+ * @Description TODO:
+ * @Author: LiZhenxiang
+ * @Creat: 2020/9/7 14:17
+ * @Update 2020/9/7 14:17
+ * @See:
+ * @Version: 1.0
+ **/
+public class TestGit {
+}
